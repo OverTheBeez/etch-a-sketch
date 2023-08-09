e6b33a07858e4f09d402d9de80f4a1a426e9a444
Jarad Overby <joverby@css.edu>
2023-08-09 14:28:31 -0500

    Working version of Etch-a-Sketch

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const canvasContainer = document.querySelector(".canvas-container");
+console.log(canvasContainer);
+
+const createCanvas = function () {
+  for (let i = 0; i < 64; i++) {
+    const canvasBox = document.createElement("div");
+
+    canvasBox.classList.add("canvas-box");
+    canvasContainer.appendChild(canvasBox);
+  }
+
+  const boxes = document.querySelectorAll(".canvas-box");
+  //   console.log(boxes);
+
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", function (e) {
+      //   console.log("Hovered over box");
+      //   e.target.style["background-color"] = "blue";
+      e.target.classList.toggle("color-change");
+    });
+  });
+};
+
+createCanvas();
